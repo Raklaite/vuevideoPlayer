0e2f68bb731c00967b2eca8ef5da8653a752f74c
Raklaite <Raklaite@gmail.com>
2018-01-20 02:02:41 +0100

    errors fix & style

--- src/components/playerVideo.vue
+++ src/components/playerVideo.vue
@@ -1,58 +1,38 @@
 <template>
-  <div class="row" id="videoPlayer">
-    <div class="el-card__body">
-      <h1 class="title"> Vue Video Player</h1>
-      <video ref="player" preload="metadata" id="player" height="100%" width="auto" @click="togglePlayPause()">
+  <div clas="row" id="videoPlayer">
+    <div>
+      <h1>The Real Player</h1>
+      <video ref="player" preload="metadata" id="player" width="auto" @click="togglePlayPause()">
         <source src="static/video/big_buck_bunny.mp4" type="video/mp4">
         <!-- Text for users who can not see vdeo balise -->
         Please install a REAL like browser chrome or firefox or update yours !
       </video>
     </div>
+    <br>
     <div>
-      <!-- conditionals alerts -->
-      <el-alert class="alertFix" v-if="this.time > 15 " title="La vidéo a atteint 25% de progression" type="succes" :closable="true"
-        show-icon>
-      </el-alert>
-      <el-alert class="alertFix" v-if="this.time > 30  " title="La vidéo a atteint 50% de progression" type="succes" :closable="true"
-        show-icon>
-      </el-alert>
-      <el-alert class="alertFix" v-if="this.time > 45  " title="La vidéo a atteint 75% de progression" type="succes" :closable="true"
-        show-icon>
-      </el-alert>
-      <el-alert class="alertFix" v-if="vid.ended" title="La vidéo est finie " type="succes" :closable="true" show-icon>
-      </el-alert>
+
+      <!-- methods/ utility bars -->
       <el-card class="box-card">
         <div slot="header" class="clearfix">
           <span>Controles name</span>
           <br>
-          <el-input-number v-model="this.time" :disabled="true"></el-input-number>
-          <!-- View time -->
-          <br>: {{this.time}} s
-        </div>
-        <div class="text item">
-          <!-- pop over content -->
-          <el-popover ref="restartPlayer" placement="top" width="10rem" trigger="hover" content="Click to restart the video"></el-popover>
-          <el-popover ref="togglePlayPause" placement="top" width="10rem" trigger="hover" content="Click to to play or pause the video"></el-popover>
-          <el-popover ref="toggleFullScreen" placement="top" width="10rem" trigger="hover" content="Click for fullScreen mode"></el-popover>
-          <el-popover ref="toggleMute" placement="top" width="10rem" trigger="hover" content="Click to Mute/Unmute "></el-popover>
-          <el-button v-popover:restartPlayer @click="restartPlayer()" type="primary" icon="fa fa-refresh"></el-button>
-          <el-button v-popover:togglePlayPause label="viewTime" v-if="playing === false" @click="togglePlayPause()" type="primary"
-            icon="fa fa-play"></el-button>
-          <el-button v-popover:togglePlayPause v-if="playing === true" @click="togglePlayPause()" type="primary" icon="fa fa-stop"></el-button>
-          <el-button v-popover:toggleFullScreen @click="toggleFullScreen()" type="primary" icon="fa fa-arrows-alt"></el-button>
-          <el-button v-popover:toggleMute v-if="isMute === true" @click="toggleMute()" type="primary" icon="fa fa-volume-off"></el-button>
-          <el-button v-popover:toggleMute v-if="isMute === false" @click="toggleMute()" type="primary" icon="fa fa-volume-up"></el-button>
-          <!-- TimeSlider -->
-          <el-slider type="range" v-on:change="changeTime()" v-model="seekbar">
-          </el-slider>
-        </div>
-      </el-card>
-      <!-- native HTMLprogressBar -->
-      <!-- <div class="slider player__progress-bar">
-          <input v-model="seekbar" type="range" step="0.1" v-on:change="changeTime()">
-          <label v-if="this.time > 5">{{this.time}}</label>
-        </div> -->
-      <!-- <label v-if="this.time > 0">{{this.time}}</label> -->
+          </div>
+          <div class="text item">
+<el-input v-model="this.time" :disabled="true" placeholder=""></el-input>
+      <div class="slider player__progress-bar">
+<el-slider v-model="seekbar" type="range" v-on:change="changeTime()" ></el-slider>
+<el-button @click="restartPlayer()" type="primary" icon="fa fa-refresh"></el-button>
+      <el-button v-if="playing === false" @click="togglePlayPause()" type="primary" icon="fa fa-play"></el-button>
+      <el-button v-if="playing === true" @click="togglePlayPause()" type="primary" icon="fa fa-stop"></el-button>
+      <el-button @click="toggleFullScreen()" type="primary" icon="fa fa-arrows-alt"></el-button>
+      <el-button v-if="isMute === true" @click="toggleMute()" type="primary" icon="fa fa-volume-off"></el-button>
+      <el-button v-if="isMute === false" @click="toggleMute()" type="primary" icon="fa fa-volume-up"></el-button>
+            <el-button  @click="toggleTest()" type="primary" icon="fa fa-volume-up"></el-button>
+        <!-- <input v-model="seekbar" type="range" step="0.1" v-on:change="changeTime()">
+        <label>{{this.time}}</label>-->
+      </div>
+          </div>
+          </el-card>
     </div>
     <br>
     <div>
@@ -78,21 +58,24 @@ export default {
     }
   },
   methods: {
+    toggleTest () {
+      console.log('Start: ' + this.vid.buffered.start(0) + ' End: ' + this.vid.buffered.end(0))
+    },
     togglePlayPause () {
+      console.log(this.vid)
       if (this.playing === false) {
-        this.updateTime()
         this.vid.play(true)
         this.playing = true
       } else {
         this.vid.pause()
         this.playing = false
       }
+      this.updateTime()
     },
     restartPlayer () {
       this.seekbar = 0
       this.vid.load()
       this.togglePlayPause()
-      this.vid.currentTime = 0
     },
     toggleFullScreen () {
       if (this.vid.mozRequestFullScreen) {
@@ -117,8 +100,8 @@ export default {
     updateTime () {
       var self = this
       self.seekbar = (self.vid.currentTime / self.vid.duration) * 100
-      self.time = self.vid.currentTime.toFixed(0)
-      setInterval(self.updateTime, 1000)
+      self.time = self.vid.currentTime.toFixed(1)
+      setInterval(self.updateTime, 10000)
     }
   },
   mounted () {
@@ -126,12 +109,13 @@ export default {
   }
 }
 </script>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style  scoped>
 .box-card{
-max-width: 50em;
-margin: 1em 33em 1em 33em;
+  min-width: 25;
+max-width: 25;
+margin: 1em 25em 1em 25em;
 
 }
-
 </style>
